b1e6600db4f866455e6e97ccc61e119bc7c63fa7
Daniel Kandalaft <kandddany1985@gmail.com>
2023-07-09 13:44:51 +0300

    Remove all Contact

--- src/Components/ContactPages/ContactIndex.jsx
+++ src/Components/ContactPages/ContactIndex.jsx
@@ -101,6 +101,14 @@ class ContactIndex extends React.Component {
     });
   };
 
+  handleRemoveAllContact = () => {
+    this.setState((prevState) => {
+      return {
+        contactList: [],
+      };
+    });
+  };
+
   render() {
     return (
       <div>
@@ -113,7 +121,9 @@ class ContactIndex extends React.Component {
               />
             </div>
             <div className="col-4 row">
-              <RemoveAllContact />
+              <RemoveAllContact
+                handleRemoveAllContact={this.handleRemoveAllContact}
+              />
             </div>
             <div className="row py-2">
               <div className="col-8 offset-2 row">
